d6ff525eb3c8389143dd5f8f7b5f28a88ff49a3c
AlvaroPaja25 <i202407963@cibertec.edu.pe>
2025-08-16 23:05:31 -0500

    primer commit

--- appRestauranteDSW_WebApi/obj/Debug/net8.0/appRestauranteDSW_WebApi.AssemblyInfo.cs
+++ appRestauranteDSW_WebApi/obj/Debug/net8.0/appRestauranteDSW_WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appRestauranteDSW_WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eba658f605dc87a480080043df9fb07a0f1055c")]
 [assembly: System.Reflection.AssemblyProductAttribute("appRestauranteDSW_WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appRestauranteDSW_WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
