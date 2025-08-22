86b0d615f44103e766b720fa9b49a37a57e8f485
Sebastian484 <sebastianazucenacc@gmail.com>
2025-08-22 14:04:04 -0500

    Crud de Mesas

---
+++ appRestauranteDSW_CoreMVC/Models/Mesas.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace appRestauranteDSW_CoreMVC.Models
+{
+    public class Mesas
+    {
+        [Display(Name = "Id del Usuario")] public int? id { get; set; }
+        [Display(Name = "Cantidad de Asientos")] public int? cantidad_asientos { get; set; }
+        [Display(Name = "Estado de la Mesa")] public string? estado { get; set; }
+    }
+}
