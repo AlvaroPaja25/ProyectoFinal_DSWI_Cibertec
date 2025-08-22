71cf6c9ebdb92fbe492c7d17f2484c7790c778b2
AlvaroPaja25 <i202407963@cibertec.edu.pe>
2025-08-21 22:46:20 -0500

    payasos controller

---
+++ appRestauranteDSW_CoreMVC/Controllers/PayasosController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appRestauranteDSW_CoreMVC.Controllers
+{
+    public class PayasosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
