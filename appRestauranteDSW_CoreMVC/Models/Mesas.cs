e34020a93f38f3484d3fada7d2db6542ad2ea321
Sebastian484 <sebastianazucenacc@gmail.com>
2025-08-22 15:54:57 -0500

    Se agrego establecminetos y boostrap

--- appRestauranteDSW_CoreMVC/Models/Mesas.cs
+++ appRestauranteDSW_CoreMVC/Models/Mesas.cs
@@ -4,7 +4,7 @@ namespace appRestauranteDSW_CoreMVC.Models
 {
     public class Mesas
     {
-        [Display(Name = "Id del Usuario")] public int? id { get; set; }
+        [Display(Name = "Id de Mesas")] public int? id { get; set; }
         [Display(Name = "Cantidad de Asientos")] public int? cantidad_asientos { get; set; }
         [Display(Name = "Estado de la Mesa")] public string? estado { get; set; }
     }
